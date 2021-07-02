1dd3011eddadc80d3653d5127bad7ed49ad73303
dskim9882 <dskim9882@gmail.com>
2021-07-02 18:50:30 +0900

    초기 커밋.

---
+++ AccountsManagement/BankingSystemVer1.cpp
@@ -0,0 +1,31 @@
+/*
+Banking System Ver 0.1
+Author : dskim9882, email - dskim9882@gmail.com
+Contents : level 1 of Accounts Management Project
+Reference : 열혈 C++ 프로그래밍, 윤성우 저
+*/
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+void showMenu();
+
+int main() {
+	
+	while (1) {
+		
+	}
+	
+	return 0;
+}
+
+void showMenu() {
+	cout << "-----MENU-----" << endl;
+	cout << "1. Create Account" << endl;
+	cout << "2. Deposit Money" << endl;
+	cout << "3. Withdraw Money" << endl;
+	cout << "4. List Accounts" << endl;
+	cout << "5. Exit" << endl;
+	cout << "Choice > ";
+}
